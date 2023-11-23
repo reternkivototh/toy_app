b4366f4666d1acc4cb0ad25b82ad202e98f56d8b
ReturnKivototh <126948762+reternkivototh@users.noreply.github.com>
2023-11-23 07:23:38 +0000

    [Add] controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello, world!"
   end
+
+  def index
+    @user = User.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
